de6b78037453a3595a502d4dad3c2255889b2d14
YoeongGun <yo941@naver.com>
2023-11-24 17:30:46 +0900

    1124

--- 2023-11-24/static/javascript/2dm.js
+++ 2023-11-24/static/javascript/2dm.js
@@ -3,9 +3,17 @@ let image = ["딸기.jfif","바나나.jfif","복숭아.jfif","아보카도.jfif"
 let board = new Array(); // 게임판 
 
 let box=0;
-
+let clk = 0; // 클릭 횟수
+let click_img=0; // 첫번재 클릭한 이미지
 $(function(){
-   let box = 0;
+   
+    box = $(".pic").length;
+    
+    let box=0;
+    let clk = 0; //클릭 횟수
+    let click_img //첫번째 클릭한 이미지
+    let pic = {-1,-1}; // 서로다른이미지 선택한경우
+    let same=false; // 같은 이미지 찾은 경우 감추기 안되게  
 });
 
 $(function(){
@@ -25,8 +33,31 @@ $(function(){
 
     for( var i=0; i< board.length; i++){
         for( var k=0; k< board[i].length; k++){
-            $(".pic").eq( i * board[i].length+k ).find("img").attr("src","./static/images/"+image[board[i][k]]);
+            $(".pic").eq( i * board[i].length+k ).
+            find("img").attr("src","./static/images/"+image[board[i][k]]);
         }
     }
+    $(".pic").on("click",function(){
+        clk++;
+        var th = $(".pic").index($(this));
+        
+        if(clk == 1) {
+            if( pic[0]!= -1){
+                $(".pic").eq(pic[0]).find("img").hide();
+                $(".pic").eq(pic[0]).find("img").hide();
+                pic - [-1,-1];
+            }
+            click_img = board[parseInt(th/parseInt(box/2))][th%parseInt(box/2)];
+            pic = th;
+        
+            if(clk==2){
+            var sec = board[parseInt(th/parseInt(box/2))][th%parseInt(box/2) ];
+            pic[1] = th;
+            if (click_img -- sec )
+                alert("같은 이미지 찾음");
+                clk=0;
+            click_img=0;
+        }
+    });
 
 });
