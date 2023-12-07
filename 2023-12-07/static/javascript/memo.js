b1d5cfb737dfe6e1fe81c4c01f2972e70d1e17d7
YoeongGun <yo941@naver.com>
2023-12-07 12:01:31 +0900

    1207

---
+++ 2023-12-07/static/javascript/memo.js
@@ -0,0 +1,12 @@
+// <i calss="bi bi-trash"></i>
+
+//생성자 함수
+function memo ( num, content , importance, ){
+    this.num=num;
+    this.content=content;
+    this.importance=importance;
+}
+function importStyle(text, color ){
+    this.text=text;
+    this.color=color;
+}
